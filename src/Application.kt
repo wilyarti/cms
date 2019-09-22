f4073089587bb9b37638126d47a67126a7a2e5ee
Wilyarti Howard <wilyarti@gmail.com>
2019-09-22 11:33:07 +1000

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,63 @@
+package os3
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.gson.*
+import dynamicPagesAPI
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module() {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        dynamicPagesAPI()
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+    }
+}
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
---
+++ src/dynamicPagesAPI.kt
@@ -0,0 +1,189 @@
+import io.ktor.application.call
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.html.*
+import io.ktor.http.Parameters
+import io.ktor.http.parametersOf
+import io.ktor.response.respond
+import kotlinx.css.div
+import kotlinx.html.*
+import net.opens3.db_password
+import net.opens3.db_username
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object pages : Table() {
+    val id = pages.integer("id").autoIncrement().primaryKey() // Column<Int>
+    val name = pages.varchar("name", length = 150) // Column<String>
+    val icon = pages.varchar("icon", length = 150) // Column<String>
+}
+
+object posts : Table() {
+    val id = posts.integer("id").autoIncrement().primaryKey() // Column<Int>
+    val pageID = posts.integer("pageID").nullable()
+    val name = posts.varchar("name", length = 150) // Column<String>
+    val contents = posts.text("contents") // Column<String>
+}
+
+data class thisPost(
+    val id: Int,
+    val pageID: Int?,
+    val name: String,
+    val contents: String
+)
+
+data class completePage(
+    val id: Int,
+    val name: String,
+    val icon: String,
+    val posts: MutableList<thisPost>
+)
+
+data class pageList(
+    val id: Int,
+    val name: String,
+    val icon: String
+)
+
+
+fun connectToDB(): Unit {
+    Database.connect(
+        "jdbc:mysql://127.0.0.1:3306/webappsadmin",
+        "com.mysql.jdbc.Driver",
+        user = db_username,
+        password = db_password
+    )
+}
+
+fun Route.dynamicPagesAPI() {
+    get("/{page}") {
+        val queryParameters: Parameters = call.request.queryParameters
+        val requestedPageNumber: String? = call.request.queryParameters["page"]
+        var pageNumber = 1
+        if (requestedPageNumber !== null) {
+            pageNumber = requestedPageNumber!!.toInt()
+        }
+        var pageData = getPages()
+        println("pagenumber ${pageNumber}")
+        call.respondHtml {
+            head {
+                link(
+                    rel = "stylesheet",
+                    href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+                    type = "text/css"
+                )
+            }
+            body {
+                div(classes = "container") {
+                    nav {
+                        classes = setOf("navbar", "navbar-expand-lg", "navbar-light", "bg-light")
+                        a(href = "#") {
+                            classes = setOf("navbar-brand")
+                            img(src = "/static/favicon.ico") {
+                                attributes["width"] = "30"
+                                attributes["height"] = "30"
+                                attributes["alt"] = ""
+                            }
+                        }
+                        div {
+                            attributes["id"] = "navbarNav"
+                            classes = setOf("collapse", "navbar-collapse")
+                            ul {
+                                classes = setOf("navbar-nav")
+                                for (page in pageData) {
+                                    li {
+                                        if (page.id == pageNumber ) {
+                                            classes =setOf("nav-item active")
+                                        } else {
+                                            classes = setOf("nav-item")
+                                        }
+                                        a(href = "/&?page=${page.id}") {
+                                            classes = setOf("nav-link")
+                                            +page.name
+                                            /*span {
+                                                classes = setOf("sr-only")
+                                            }*/
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    div {
+                        div {
+                            h1 {
+                                +pageData[pageNumber-1].name
+                            }
+                            "Posts:"
+                            div {
+                                for (post in pageData[pageNumber-1].posts) {
+                                    h4 {
+                                        +post.name
+                                    }
+                                    unsafe {
+                                        +post.contents
+                                    }
+                                }
+                            }
+                            br {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+    get("/getPageList") {
+        call.respond(getPages())
+    }
+}
+
+fun getPages(): MutableList<completePage> {
+    connectToDB()
+    var returnedPages = mutableListOf<completePage>()
+    transaction {
+        SchemaUtils.create(pages, posts)
+        val allPosts = posts.selectAll()
+        for (page in pages.selectAll()) {
+            val currentPage = completePage(
+                id = page[pages.id],
+                name = page[pages.name],
+                icon = page[pages.icon],
+                posts = mutableListOf<thisPost>()
+            )
+            for (p in allPosts) {
+                if (p[posts.pageID] == page[pages.id]) {
+                    var currentPost = thisPost(
+                        id = p[posts.id],
+                        pageID = p[posts.pageID],
+                        name = p[posts.name],
+                        contents = p[posts.contents].toString()
+                    )
+                    currentPage.posts.add(currentPost)
+                }
+            }
+            returnedPages.add(currentPage)
+        }
+    }
+    return returnedPages
+}
+
+fun getPageList(): MutableList<pageList> {
+    connectToDB()
+    var returnedPages = mutableListOf<pageList>()
+    transaction {
+        SchemaUtils.create(pages, posts)
+        val allPosts = posts.selectAll()
+        for (page in pages.selectAll()) {
+            val currentPage = pageList(
+                id = page[pages.id],
+                name = page[pages.name],
+                icon = page[pages.icon]
+            )
+            returnedPages.add(currentPage)
+        }
+    }
+    return returnedPages
+}
