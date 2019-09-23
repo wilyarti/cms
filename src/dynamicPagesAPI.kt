ff21f5720e2c2ef2ca99ca4c7bde67b923fcbcfd
Wilyarti Howard <wilyarti@gmail.com>
2019-09-23 18:08:56 +1000

    continued development

--- src/dynamicPagesAPI.kt
+++ src/dynamicPagesAPI.kt
@@ -20,29 +20,75 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import os3.respondCss
 
 object pages : Table() {
-    val id = pages.integer("id").autoIncrement().primaryKey() // Column<Int>
-    val name = pages.varchar("name", length = 150) // Column<String>
-    val icon = pages.varchar("icon", length = 150) // Column<String>
+    val id = pages.integer("id").autoIncrement().primaryKey() // main id
+    val disabled = pages.bool("disabled")
+    val parentID = pages.integer("pid") // allow tree structure (stipulate parent)
+    val priorityBit = pages.integer("priorityBit") // to re-order pages and sticky bit
+    val name = pages.varchar("name", length = 150) // page name
+    val icon = pages.varchar("icon", length = 150)  // feather icon
+    val pageID = pages.integer("pageID")
+    val author = pages.varchar("author", length = 150)
+    val group = pages.varchar("group", length = 150)
+    val createdTime = posts.varchar("createdTime", length = 150) // date, time and timezone
+    val countryOfOrigin = pages.varchar("countryOfOrigin", length = 8)
+    val language = pages.varchar("language", length = 150)
+    val executionScript = pages.text("executionScript")
+    val contents = pages.text("contents")
+    val metadata = pages.text("metadata")
 }
 
 object posts : Table() {
-    val id = posts.integer("id").autoIncrement().primaryKey() // Column<Int>
-    val pageID = posts.integer("pageID").nullable()
-    val name = posts.varchar("name", length = 150) // Column<String>
-    val contents = posts.text("contents") // Column<String>
+    val id = posts.integer("id").autoIncrement().primaryKey() // main id
+    val disabled = posts.bool("disabled")
+    val parentID = posts.integer("pid") // allow tree structure (stipulate parent)
+    val priorityBit = posts.integer("priorityBit") // to re-order pages and sticky bit
+    val name = posts.varchar("name", length = 150) // page name
+    val icon = posts.varchar("icon", length = 150)  // feather icon
+    val pageID = posts.integer("pageID")
+    val author = posts.varchar("author", length = 150)
+    val group = posts.varchar("group", length = 150)
+    val createdTime = posts.varchar("createdTime", length = 150) // date, time and timezone
+    val countryOfOrigin = posts.varchar("countryOfOrigin", length = 8)
+    val language = posts.varchar("language", length = 150)
+    val executionScript = posts.text("executionScript")
+    val contents = posts.text("contents")
+    val metadata = posts.text("metadata")
 }
 
 data class thisPost(
     val id: Int,
-    val pageID: Int?,
+    val disabled: Boolean,
+    val parentID: Int,
+    val priorityBit: Int,
     val name: String,
-    val contents: String
+    val icon: String,
+    val pageID: Int,
+    val author: String,
+    val group: String,
+    val createdTime: String,
+    val countryOfOrigin: String,
+    val language: String,
+    val executionScript: String,
+    val contents: String,
+    val metadata: String
 )
 
 data class completePage(
     val id: Int,
+    val disabled: Boolean,
+    val parentID: Int,
+    val priorityBit: Int,
     val name: String,
     val icon: String,
+    val pageID: Int,
+    val author: String,
+    val group: String,
+    val createdTime: String,
+    val countryOfOrigin: String,
+    val language: String,
+    val executionScript: String,
+    val contents: String,
+    val metadata: String,
     val posts: MutableList<thisPost>
 )
 
@@ -70,7 +116,7 @@ fun Route.dynamicPagesAPI() {
         if (requestedPageNumber !== null) {
             pageNumber = requestedPageNumber!!.toInt()
         }
-        var pageData = getPages()
+        var pageData = getAllPostsAndPages()
         println("pagenumber ${pageNumber}")
         call.respondHtml {
             head {
@@ -153,15 +199,12 @@ fun Route.dynamicPagesAPI() {
             }
         }
     }
-    get("/api/getPages") {
-        call.respond(getPages())
-    }
-    get("/api/getPosts") {
-        call.respond(getPosts())
+    get("/api/getAllPostsAndPages") {
+        call.respond(getAllPostsAndPages())
     }
 }
 
-fun getPages(): MutableList<completePage> {
+fun getAllPostsAndPages(): MutableList<completePage> {
     connectToDB()
     var returnedPages = mutableListOf<completePage>()
     transaction {
@@ -170,58 +213,44 @@ fun getPages(): MutableList<completePage> {
         for (page in pages.selectAll()) {
             val currentPage = completePage(
                 id = page[pages.id],
+                disabled = page[pages.disabled],
+                parentID = page[pages.parentID],
+                priorityBit = page[pages.priorityBit],
                 name = page[pages.name],
                 icon = page[pages.icon],
+                pageID = page[pages.pageID],
+                author = page[pages.author],
+                group = page[pages.group],
+                createdTime = page[pages.createdTime].toString(),
+                countryOfOrigin = page[pages.countryOfOrigin],
+                language = page[pages.language],
+                executionScript = page[pages.executionScript].toString(),
+                contents = page[pages.contents].toString(),
+                metadata = page[pages.metadata].toString(),
                 posts = mutableListOf<thisPost>()
             )
             for (p in allPosts) {
                 if (p[posts.pageID] == page[pages.id]) {
                     var currentPost = thisPost(
                         id = p[posts.id],
-                        pageID = p[posts.pageID],
+                        disabled = p[posts.disabled],
+                        parentID = p[posts.parentID],
+                        priorityBit = p[posts.priorityBit],
                         name = p[posts.name],
-                        contents = p[posts.contents].toString()
+                        icon = p[posts.icon],
+                        pageID = p[posts.pageID],
+                        author = p[posts.author],
+                        group = p[posts.group],
+                        createdTime = p[posts.createdTime].toString(),
+                        countryOfOrigin = p[posts.countryOfOrigin],
+                        language = p[posts.language],
+                        executionScript = p[posts.executionScript].toString(),
+                        contents = p[posts.contents].toString(),
+                        metadata = p[posts.metadata].toString()
                     )
                     currentPage.posts.add(currentPost)
                 }
             }
-            returnedPages.add(currentPage)
-        }
-    }
-    return returnedPages
-}
-
-fun getPosts(): MutableList<thisPost> {
-    connectToDB()
-    var returnedPosts = mutableListOf<thisPost>()
-    transaction {
-        SchemaUtils.create(posts)
-        val allPosts = posts.selectAll()
-        for (p in allPosts) {
-            val currentPost = thisPost(
-                id = p[posts.id],
-                pageID = p[posts.pageID],
-                name = p[posts.name],
-                contents = p[posts.contents].toString()
-            )
-            returnedPosts.add(currentPost)
-        }
-    }
-    return returnedPosts
-}
-
-fun getPageList(): MutableList<pageList> {
-    connectToDB()
-    var returnedPages = mutableListOf<pageList>()
-    transaction {
-        SchemaUtils.create(pages, posts)
-        val allPosts = posts.selectAll()
-        for (page in pages.selectAll()) {
-            val currentPage = pageList(
-                id = page[pages.id],
-                name = page[pages.name],
-                icon = page[pages.icon]
-            )
             returnedPages.add(currentPage)
         }
     }
