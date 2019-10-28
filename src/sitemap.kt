1be03871ca5567e56d95cc6ba19c93cc951c133f
Wilyarti Howard <wilyarti@gmail.com>
2019-10-29 07:12:11 +1000

    Fix sitemap generator.

--- src/sitemap.kt
+++ src/sitemap.kt
@@ -1,7 +1,11 @@
 // Contains the routes for the Content Management System in the /home route.
 package os3
 
+import com.fasterxml.jackson.annotation.JacksonAnnotation
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -33,32 +37,43 @@ data class url(
     val loc: String,
     val lastmod: String,
     val changefreq: String,
+    @JsonProperty("foo")
     val priority: String
 )
 
-data class Sitemap (
-    val urlset: List<url>
-)
+/** FasterXML does not support XML Schema Declarations...
+ * https://github.com/FasterXML/jackson-dataformat-xml/issues/90
+ * So we have to build this by hand.
+ *
+ * It's not so messy as each url node is built using the XML serializer.
+ * This allows us to keep the form type checked.
+ */
 
 internal fun Route.siteMap() {
 
     get("/sitemap.xml") {
         val mapper = XmlMapper()
-        var pages = getAllPostsAndPages()
-        var urlset = mutableListOf<url>()
+        val pages = getAllPostsAndPages()
+        var ourXML = """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <!-- generator="$ourUrl" -->
+            <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        """.trimIndent()
         for ((index, page) in pages.withIndex()) {
             for ((postIndex) in page.posts.withIndex()) {
                 var thisUrl = url(
+                    //TODO create SQL table for images
+                    //TODO implement image scaping on update() and addPost() functions so the sitemap works
                     loc = "https://$ourUrl/post/${page.posts[postIndex].id}",
                     lastmod = page.posts[postIndex].createdTime,
                     changefreq = "monthly",
                     priority = "1.0"
                 )
-                urlset.add(thisUrl)
+                ourXML += mapper.writeValueAsString(thisUrl) + "\n"
             }
         }
-        val sitemap = Sitemap(urlset = urlset)
-        call.respondText(ContentType.Application.Xml) { mapper.writeValueAsString(sitemap) }
+        ourXML += "</urlset>"
+        call.respondText(ContentType.Application.Xml) { ourXML }
     }
 
 }
